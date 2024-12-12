b9e69c7ec994e50bd3246a11a90c3fbcad3e9dd9
Seth <github@sesu.tech>
2024-12-12 13:28:27 -0500

    Update and rename Remove Employee From All Users' Contacts.sh to Remove Contact From All Users' Contacts.sh

--- scripts/Remove Contact From All Users' Contacts.sh
+++ scripts/Remove Contact From All Users' Contacts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Remove Employee From All Users' Contacts
+# Remove Contact From All Users' Contacts
 # Created by: Sesu Tech <github.com/sesu-tech>
 # Created on: 2024.08.12
 # Modified on: 2024.08.12
@@ -9,7 +9,7 @@
 gam="/opt/taers232c/GAMADV-XTD/gamadv-xtd3/gam"
 
 # Prompt For Employee Email
-echo "Please enter the employee's email (ex: jdoe@company.org)"
+echo "Please enter the contact's email (ex: jdoe@company.org)"
 read email
 read -r -p  "You entered "$email", are you sure you'd like to continue? [y/N]" -n 1
 echo
@@ -19,10 +19,10 @@ case "$REPLY" in
 esac
 
 # Remove Contact
-echo "Removing Employee from All User 'Contacts'"
+echo "Removing Contact from All User 'Contacts'"
 $gam all users delete contacts emailmatchpattern "$email"
 
-echo "Removing Employee from All User 'Other Contacts'"
+echo "Removing Contact from All User 'Other Contacts'"
 $gam all users delete othercontacts emailmatchpattern "$email"
 
 echo "All done!"
